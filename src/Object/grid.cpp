a99e2bb8c158f9c4e5bf6ea2026727aa1dc5a52c
lucasfturos <lucasfturos@gmail.com>
2024-02-12 12:53:50 -0300

    Create class Grid

--- src/Object/grid.cpp
+++ src/Object/grid.cpp
@@ -18,8 +18,8 @@ void Grid::generateIndices() {
             GLint row1 = j * (num_segments + 1);
             GLint row2 = (j + 1) * (num_segments + 1);
 
-            indices.push_back(glm::uvec3(row1 + i, row1 + i + 1, row2 + i));
-            indices.push_back(glm::uvec3(row1 + i + 1, row2 + i + 1, row2 + i));
+            indices.push_back(glm::uvec3(row1 + i, row1 + i + 1, row2 + i + 1));
+            indices.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
         }
     }
 }
@@ -29,7 +29,7 @@ void Grid::generateVertices() {
         for (auto j = 0; j <= num_segments; ++j) {
             GLfloat x = static_cast<GLfloat>(i) / num_segments;
             GLfloat y = static_cast<GLfloat>(j) / num_segments;
-            GLfloat z = 0.0f;
+            GLfloat z = sin(x * 2.0f * M_PI) * sin(y * 2.0f * M_PI) * 0.1f;
             vertices.push_back(glm::vec3(x, y, z));
         }
     }
